13adfeb619b91c501f13dcb3f4769a23085ad9d7
Andy Klimczak <andyklimczak@fastmail.com>
2016-03-15 23:02:37 -0500

    pre crudify api resources

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import consign from 'consign';
 
-const PORT = 3000;
 const app = express();
 
 consign()
-  .include('models')
+  .include('libs/config.js')
+  .then('db.js')
   .then('libs/middlewares.js')
   .then('routes')
   .then('libs/boot.js')
