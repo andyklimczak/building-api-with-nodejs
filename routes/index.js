0af97c56293f0399ff3b7120b870cc504cc92f84
Andy Klimczak <andyklimczak@fastmail.com>
2016-03-21 20:23:29 -0400

    pre documenting token generation

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,11 @@
+/**
+ * @api {get} / API Status
+ * @apiGroup Status
+ * @apiSuccess {String} status API Status' Message
+ * @apiSuccessExample {json} Success
+ *  HTTP/1.1 200 OK
+ *  {"status": "Ntask API"}
+ */
 module.exports = app => {
   app.get('/', (req, res) => {
     res.json({status: 'NTask API'});
